56b2f848703f4446b6546638f76e78b75601561a
wenkesj <samwenke@gmail.com>
2018-12-10 09:55:39 -0500

    move contrib to other PR

--- pyoneer/contrib/interactor/README.md
+++
@@ -1,3 +0,0 @@
-# pynr.contrib.interactor
-
-InterActor is just _one-way_ of implementing agent interaction with an environment.
--- pyoneer/contrib/interactor/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from pyoneer.contrib.interactor.batch_gym_impl import batch_gym_make
-from pyoneer.contrib.interactor.batch_gym_impl import BatchEnv
-from pyoneer.contrib.interactor.batch_rollout_impl import batch_rollout
-from pyoneer.contrib.interactor.rollout_impl import Rollout
