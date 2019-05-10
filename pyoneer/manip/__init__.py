e189f6df39ff67512eda0b4d068272e10e745c81
Jim Fleming <jimmy.fleming@gmail.com>
2019-05-09 18:32:30 -0700

    Add flatten op

--- pyoneer/manip/__init__.py
+++ pyoneer/manip/__init__.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from pyoneer.manip.array_ops import pad_or_truncate, shift
+from pyoneer.manip.array_ops import flatten, pad_or_truncate, shift
 from pyoneer.manip.indexing_ops import batched_index
