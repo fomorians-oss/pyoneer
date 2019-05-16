ac478b68c8a8cc87f979f7b182f9b63bd91b7629
Jim Fleming <jimmy.fleming@gmail.com>
2019-05-16 10:04:38 -0700

    Remove process and batch envs

--- pyoneer/rl/__init__.py
+++ pyoneer/rl/__init__.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from pyoneer.rl import envs
 from pyoneer.rl import losses
 from pyoneer.rl import strategies
 from pyoneer.rl import targets
--- pyoneer/rl/envs/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from pyoneer.rl.envs.batch_env_impl import BatchEnv
-from pyoneer.rl.envs.process_env_impl import ProcessEnv
