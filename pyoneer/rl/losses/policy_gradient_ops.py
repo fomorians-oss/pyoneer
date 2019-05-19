8417d0970a5b0b9876ac035129e774ea3cf9aa51
Jim Fleming <jimmy.fleming@gmail.com>
2019-05-19 09:16:48 -0700

    Remove unused import

--- pyoneer/rl/losses/policy_gradient_ops.py
+++ pyoneer/rl/losses/policy_gradient_ops.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import abc
 import tensorflow as tf
 
 from tensorflow.python.keras.utils import losses_utils
