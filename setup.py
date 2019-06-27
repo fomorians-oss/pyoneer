4893da20d7a9d9f02674b252b16d8489e7424e99
Jim Fleming <jimmy.fleming@gmail.com>
2019-06-27 12:52:36 -0700

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -2,40 +2,43 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-
 from setuptools import setup, find_packages
 
-REQUIRED_PACKAGES = ["gym"]
-README_PATH = os.path.join(os.path.dirname(__file__), "README.md")
-
-with open(README_PATH, "r") as fp:
-    README = fp.read()
+install_requires = ["gym"]
+with open("README.md", "r") as fp:
+    long_description = fp.read()
 
 setup(
     name="fomoro-pyoneer",
-    version="0.1.0",
-    description="""
-    Tensor utilities, reinforcement learning, and more!
-    Designed to make research easier with low-level abstractions for common operations.
-    """,
+    version="0.2.0",
     author="Fomoro AI",
     author_email="team@fomoro.com",
-    long_description=README,
+    description="Tensor utilities, reinforcement learning, and more!",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    license="Apache 2.0",
     url="https://github.com/fomorians/pyoneer",
-    install_requires=REQUIRED_PACKAGES,
-    extras_require={"dev": ["pre-commit"]},
+    license="Apache 2.0",
     packages=find_packages(),
+    install_requires=install_requires,
+    extras_require={"dev": ["pre-commit"]},
     include_package_data=True,
+    keywords=[
+        "tensorflow",
+        "machine learning",
+        "reinforcement learning",
+        "eager execution",
+        "deep learning",
+    ],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
     ],
 )
