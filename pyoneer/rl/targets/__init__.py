e3d2cd31cb572c51b695391c461f3605ff7febeb
Jim Fleming <jimmy.fleming@gmail.com>
2019-08-16 13:08:27 -0700

    Remove sequence ops

--- pyoneer/rl/targets/__init__.py
+++ pyoneer/rl/targets/__init__.py
@@ -3,6 +3,5 @@ from __future__ import division
 from __future__ import print_function
 
 from pyoneer.rl.targets.target_ops import DiscountedReturns, GeneralizedAdvantages
-from pyoneer.rl.targets.sequence_ops import scan_discounted_sum, multistep_forward_view
 
 __all__ = ["DiscountedReturns", "GeneralizedAdvantages"]
